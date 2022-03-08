65e8722177c4de3359510f975bb6a3a07f2006ff
Sander De la Marche <sander@sumocoders.be>
2022-03-08 14:22:23 +0100

    Add more checks to command

--- src/Command/BuildSiteCommand.php
+++ src/Command/BuildSiteCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use DOMDocument;
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -9,6 +10,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\Translation\Translator;
 use Twig\Environment;
 
@@ -16,13 +18,11 @@ class BuildSiteCommand extends Command
 {
     private const SOURCE_FOLDER = 'source';
     private const OUTPUT_FOLDER = 'output';
-    private const LOCALES = [
-        'nl',
-        'fr'
-    ];
 
     public function __construct(
-        private Environment $twig
+        private Environment $twig,
+        private Filesystem $filesystem,
+        private array $config
     ) {
         parent::__construct();
     }
@@ -34,33 +34,36 @@ class BuildSiteCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $fileSystem = new Filesystem();
         $finder = new Finder();
+
+        $this->filesystem->remove(self::OUTPUT_FOLDER);
+        $this->filesystem->mkdir(self::OUTPUT_FOLDER);
 
         $finder->in(self::SOURCE_FOLDER)->depth(0)->files()->name('*.twig');
 
         $progressBar = new ProgressBar($output, count($finder));
         $progressBar->start();
 
-        foreach (self::LOCALES as $locale) {
-            $this->setLocale($locale);
+        if ($this->isMultiLang()) {
+            foreach ($this->config['locales'] as $locale) {
+                $this->setLocale($locale);
 
+                foreach ($finder as $file) {
+                    $this->renderTemplate($file, $locale);
+
+                    $progressBar->advance();
+                }
+            }
+        } else {
             foreach ($finder as $file) {
-                $renderedTemplate = $this->twig->render($file->getFilename());
-
-                $renderedTemplate = str_replace('assets/', '../assets/', $renderedTemplate);
-
-                $fileSystem->dumpFile(
-                    self::OUTPUT_FOLDER . '/' . $locale . '/' . $file->getFilenameWithoutExtension() . '.html',
-                    $renderedTemplate
-                );
+                $this->renderTemplate($file);
 
                 $progressBar->advance();
             }
         }
 
         // TODO: CSS? Do we need Sass/JS transpile?
-        $fileSystem->mirror(self::SOURCE_FOLDER . '/assets', self::OUTPUT_FOLDER . '/assets');
+        $this->filesystem->mirror(self::SOURCE_FOLDER . '/assets', self::OUTPUT_FOLDER . '/assets');
 
         $this->compressImages(self::OUTPUT_FOLDER . '/assets/images');
 
@@ -69,16 +72,42 @@ class BuildSiteCommand extends Command
         return Command::SUCCESS;
     }
 
+    private function renderTemplate(SplFileInfo $file, ?string $locale = null)
+    {
+        $renderedTemplate = $this->twig->render($file->getFilename());
+
+        $renderedTemplate = str_replace('assets/', '../assets/', $renderedTemplate);
+
+        if ($locale !== null) {
+            $renderedTemplate = $this->makeInternalLinksLocaleAware($renderedTemplate, $locale);
+
+            $outputPath = sprintf(
+                '%s/%s/%s.html',
+                self::OUTPUT_FOLDER,
+                $locale,
+                $file->getFilenameWithoutExtension()
+            );
+        } else {
+            $outputPath = sprintf(
+                '%s/%s.html',
+                self::OUTPUT_FOLDER,
+                $file->getFilenameWithoutExtension()
+            );
+        }
+
+        $this->filesystem->dumpFile($outputPath, $renderedTemplate);
+    }
+
     private function setLocale(string $locale): void
     {
-        assert($this->twig->getExtension(TranslationExtension::class)->getTranslator() instanceof Translator);
-        $this->twig->getExtension(TranslationExtension::class)->getTranslator()->setLocale($locale);
+        /** @var Translator $translator */
+        $translator = $this->twig->getExtension(TranslationExtension::class)->getTranslator();
+        $translator->setLocale($locale);
     }
 
     private function compressImages(string $imageFolder): void
     {
         $finder = new Finder();
-
         $finder->in($imageFolder)->files();
 
         foreach ($finder as $image) {
@@ -88,5 +117,34 @@ class BuildSiteCommand extends Command
                 shell_exec('optipng -o2 -i 0 -strip all -silent ' . $image->getRealPath());
             }
         }
+    }
+
+    private function makeInternalLinksLocaleAware(string $html, string $locale): string
+    {
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument;
+        $dom->loadHTML($html);
+        libxml_use_internal_errors(false);
+
+        $nodes = $dom->getElementsByTagName('a');
+
+        foreach ($nodes as $node) {
+            $link = $node->getAttribute('href');
+            if (str_contains($link, '.html')) {
+                $node->setAttribute('href', '/' . $locale . '/' . $link);
+            }
+        }
+
+        return $dom->saveHTML();
+    }
+
+    private function isMultiLang(): bool
+    {
+        if (array_key_exists('locales', $this->config) &&
+            count($this->config['locales']) > 1) {
+            return true;
+        }
+
+        return false;
     }
 }
