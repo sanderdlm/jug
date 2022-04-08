73ba00ca9f2dc8bff5e700051fb752270e621b08
Sander De la Marche <sander@sumocoders.be>
2022-04-08 22:44:54 +0200

    Add a simple sqlite extension

---
+++ src/Twig/SqliteExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jug\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class SqliteExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('data', [$this, 'loadData']),
+        ];
+    }
+
+    /**
+     * @return array<mixed>|null
+     */
+    public function loadData(string $databaseFile, string $query): ?array
+    {
+        if (!file_exists($databaseFile)) {
+            throw new \RuntimeException($databaseFile . ' does not exist.');
+        }
+
+        $database = new \PDO("sqlite:$databaseFile");
+
+        $database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        $result = $database->query($query);
+
+        if ($result) {
+            return $result->fetchAll();
+        }
+
+        return null;
+    }
+}
