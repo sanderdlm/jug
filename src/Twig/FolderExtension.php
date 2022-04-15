44c317a7c39606c2b07fd453e7534fcc33d611bb
Sander De la Marche <sander@sumocoders.be>
2022-04-15 11:24:45 +0200

    Add the foder Twig extension

---
+++ src/Twig/FolderExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jug\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Symfony\Component\Finder\Finder;
+
+class FolderExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('dir', [$this, 'getContents']),
+        ];
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getContents(string $path): array
+    {
+        $files = [];
+        $finder = new Finder();
+
+        $finder->in($path)->files()->name('*.twig');
+
+        foreach ($finder as $file) {
+            $files[] = $file->getFilenameWithoutExtension() . '.html';
+        }
+
+        return $files;
+    }
+}
