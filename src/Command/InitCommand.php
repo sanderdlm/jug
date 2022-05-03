6087cbe683e00cbdbac6689b0c6ce328c34f4628
Sander De la Marche <sander@sumocoders.be>
2022-05-03 16:38:16 +0200

    Fine-tune the init command

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -33,16 +33,67 @@ class InitCommand extends Command
 
         $output->write('Setting up your new Jug site..');
 
-        $defaultConfig = file_get_contents('vendor/dreadnip/jug/Fixture/default-config.txt');
-
-        if ($defaultConfig) {
-            $this->filesystem->appendToFile('config.php', $defaultConfig);
-        }
+        $this->filesystem->appendToFile('config.php', $this->getDefaultConfig());
 
         $this->filesystem->mkdir('source/_templates');
+        $this->filesystem->mkdir('source/assets/images');
+        $this->filesystem->mkdir('source/assets/css');
+        $this->filesystem->mkdir('source/assets/js');
+
+        $this->filesystem->appendToFile('source/assets/css/style.css', '');
+        $this->filesystem->appendToFile('source/_templates/base.twig', $this->getDefaultBaseTemplate());
+        $this->filesystem->appendToFile('source/index.twig', $this->getDefaultIndexTemplate());
 
         $output->writeln(' <info>Done!</info>');
 
         return Command::SUCCESS;
+    }
+
+    private function getDefaultConfig(): string
+    {
+        return <<<DOC
+        <?php
+        
+        return [
+            'source' => 'source',
+            'output' => 'output',
+            'default_locale' => 'en',
+            'hash' => bin2hex(random_bytes(4)),
+            'image_cache' => 'images.json',
+        ];
+        DOC;
+    }
+
+    private function getDefaultBaseTemplate(): string
+    {
+        return <<<DOC
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Your new site</title>
+            <meta name="description" content="Your new site">
+            <meta name="author" content="You">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/style.css') }}">
+        </head>
+        <body>
+            {% block content %}{% endblock %}
+        </body>
+        </html>
+        DOC;
+    }
+
+
+    private function getDefaultIndexTemplate(): string
+    {
+        return <<<DOC
+        {% extends 'base.twig' %}
+        
+        {% block content %}
+            <h1>Hello world</h1>
+            <p>Welcome to your new site</p>
+        {% endblock %}
+        DOC;
     }
 }
