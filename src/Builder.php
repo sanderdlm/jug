cab7f073e400af9f0f623417b1348737faa3dc81
Sander De la Marche <sander@sumocoders.be>
2022-05-11 00:28:54 +0200

    Add a Builder class that builds the Site object

---
+++ src/Builder.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jug;
+
+use Jug\Config\Config;
+use Jug\Domain\File;
+use Jug\Domain\Page;
+use Jug\Domain\Site;
+use Jug\Twig\Parser;
+use Symfony\Component\Finder\Finder;
+
+final class Builder
+{
+    public function __construct(
+        private readonly Config $config,
+        private readonly Parser $parser
+    ) {
+    }
+
+    public function build(): Site
+    {
+        return new Site($this->config, $this->collectPages());
+    }
+
+    /**
+     * @return array<Page>
+     */
+    private function collectPages(): array
+    {
+        $pages = [];
+
+        $finder = (new Finder())
+            ->in($this->config->getString('source'))
+            ->exclude('_templates')
+            ->files()
+            ->name('*.twig');
+
+        foreach ($finder as $file) {
+            $output = $this->buildLocaleAwareOutputPath($file->getRelativePathname());
+
+            $pages[] = new Page(
+                new File($file->getRelativePathname()),
+                $output,
+                $this->parser->parse($file->getRelativePathname())
+            );
+        }
+
+        return $pages;
+    }
+
+    /**
+     * @return File|array<File>
+     */
+    private function buildLocaleAwareOutputPath(string $relativePath): File|array
+    {
+        if ($this->config->has('locales')) {
+            $output = [];
+            foreach ($this->config->getArray('locales') as $locale) {
+                $outputPath = $this->buildOutputPath($relativePath);
+                $output[$locale] = new File($outputPath);
+            }
+        } else {
+            $outputPath = $this->buildOutputPath($relativePath);
+            $output = new File($outputPath);
+        }
+
+        return $output;
+    }
+
+    public function buildOutputPath(string $relativePath, ?string $locale = null): string
+    {
+        $outputFileName = str_replace('twig', 'html', $relativePath);
+
+        if ($locale !== null) {
+            $outputFileName = $locale . DIRECTORY_SEPARATOR . $outputFileName;
+        }
+
+        return $outputFileName;
+    }
+}
