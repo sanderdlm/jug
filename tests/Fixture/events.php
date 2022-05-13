d405b2fa1653e5523e3471e82501f151af774538
Sander De la Marche <sander@sumocoders.be>
2022-05-13 20:36:15 +0200

    Expand tests

--- tests/Fixture/events.php
+++ tests/Fixture/events.php
@@ -12,15 +12,35 @@ return static function (EventDispatcher $dispatcher): void {
         /** @var BeforeBuild $beforeBuild */
         $beforeBuild = $event;
 
-        foreach ($beforeBuild->site->pages as $page) {
+        /*
+         * An example of what you can build with the before event would be a tag cloud.
+         */
+        $tags = [];
+        foreach ($beforeBuild->site->select('tags') as $page) {
+            if (
+                array_key_exists('tags', $page->context) &&
+                is_array($page->context['tags'])
+            ) {
+                foreach ($page->context['tags'] as $tag) {
+                    $tags[] = $tag;
+                }
+            }
         }
+
+        $beforeBuild->site->config->add('tags', $tags);
     });
 
     $dispatcher->addListener(AfterBuild::NAME, function (Event $event) {
         /** @var AfterBuild $afterBuild */
         $afterBuild = $event;
 
+        /*
+         * An example of what you can build with the after event would be a sitemap
+         */
+
+        $sitemap = new SomeSiteMapLibrary();
         foreach ($afterBuild->site->pages as $page) {
+            $sitemap->addPage($page->output->relativePath);
         }
     });
 };
