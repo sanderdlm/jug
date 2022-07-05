9b2bc02904bdf6c663d23876bf35b4e587e41cf4
Sander De la Marche <sander@sumocoders.be>
2022-07-06 00:09:46 +0200

    Fix typo

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -9,6 +9,6 @@ class EventTest extends BaseFunctionalTest
         $fixtureTags = ['he', 'is', 'a', 'smooth', 'operator'];
 
         $this->assertIsArray($this->site->config->getarray('tags'));
-        $this->assertEquals($fixtureTags, $this->site->config->getarray('tags'));
+        $this->assertEquals($fixtureTags, $this->site->config->getArray('tags'));
     }
 }
