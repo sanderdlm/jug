bea34ff1f1819f33835409d278a17bcf87cc5ed5
Sander De la Marche <sander@sumocoders.be>
2022-07-07 14:23:22 +0200

    Add a stopwatch timer around the build command

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 #[AsCommand(
     name: 'build',
@@ -24,13 +25,18 @@ class BuildCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $stopwatch = new Stopwatch();
+        $stopwatch->start('build');
+
         $generator = $this->kernel->buildGenerator();
 
         $output->write('Building site..');
 
         $generator->generate();
 
-        $output->writeln(' <info>Done!</info>');
+        $buildTime = $stopwatch->stop('build');
+
+        $output->writeln(' <info>Done! (in ' . $buildTime . 's)</info>');
 
         return Command::SUCCESS;
     }
